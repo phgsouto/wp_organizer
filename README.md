8a53f2066c3619f92e6b31244b02f82954e837e1
Pedro H G Souto <phgsouto@gmail.com>
2018-02-08 16:32:33 -0200

    Initial commit. Also version 0.1.

---
+++ README.md
@@ -0,0 +1,12 @@
+# wp_organizer - A wallpaper organizer!
+
+Organize wallpapers by image ratio (16:9, 16:8, 21:9, etc).
+
+Dependencies: pillow
+
+```pip install pillow```
+```python wp_organizer.py```
+
+Choose default directories by changing the ```WP_TEMP_DIR``` and ```WP_FINAL_DIR``` variables in the beginning of the script.
+
+Enjoy!
---
+++ wp_organizer.py
@@ -0,0 +1,93 @@
+'''
+    wp_organizer.py - A wallpaper organizer
+    Version: 0.1
+'''
+
+import os
+from PIL import Image
+
+WP_TEMP_DIR = 'E:\\Pedro\\Pictures\\Wallpapers\\temp'
+WP_FINAL_DIR = 'E:\\Pedro\\Pictures\\Wallpapers'
+
+
+def main():
+    print('~~ wp_organizer -- A wallpaper organizer! ~~')
+    temp_wallpapers = os.listdir(WP_TEMP_DIR)
+
+    image_and_ratios = {}
+
+    for f in temp_wallpapers:
+        if not is_image(f):
+            temp_wallpapers.remove(f)
+        else:
+            image_and_ratios[f] = get_ratio(
+                get_img_dimensions(WP_TEMP_DIR + '\\' + f))
+
+    for image, ratio in image_and_ratios.items():
+        move_files(image, ratio)
+
+
+def is_image(filename=None):
+    if type(filename).__name__ == 'str':
+        if filename.split('.')[-1] in ['png', 'jpg', 'jpeg']:
+            return True
+    return False
+
+
+def get_img_dimensions(path=None):
+    size = (0, 0)
+    if path and os.path.isfile(path):
+        img = Image.open(path)
+        size = img.size
+        img.close()
+    return size
+
+
+def get_ratio(size):
+    common_ratios = {
+        1.20: '6_5',
+        1.25: '5_4',
+        1.33: '4_3',
+        1.36: '11_8',
+        1.37: '11_8',
+        1.50: '3_2',
+        1.51: '3_2',
+        1.58: '16_10',
+        1.59: '16_10',
+        1.60: '16_10',
+        1.66: '5_3',
+        1.78: '16_9',
+        2.35: '21_9',
+        2.36: '21_9',
+        2.37: '21_9',
+        2.38: '21_9',
+        2.39: '21_9',
+        2.40: '21_9',
+        0.56: '9_16',
+        0.62: '10_16',
+        0.42: '9_21'
+    }
+    if type(size).__name__ == 'tuple':
+        ratio = float("{0:.2f}".format(size[0] / size[1]))
+        if ratio in common_ratios:
+            return common_ratios[ratio]
+        else:
+            return 'unusual'
+    return -1
+
+
+def move_files(filename, directory):
+    dir_path = WP_FINAL_DIR + '\\' + directory
+    file_old_path = WP_TEMP_DIR + '\\' + filename
+    file_new_path = dir_path + '\\' + filename
+
+    if not os.path.exists(dir_path):
+        os.makedirs(dir_path)
+
+    os.rename(file_old_path, file_new_path)
+
+
+if __name__ == '__main__':
+    main()
+
+# EOF
